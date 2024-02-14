5a492a59523f0c3ae37f95c12f93cb4ebd6cc718
Elliot Brooks <elliot.brooks@sky.uk>
2024-02-14 14:11:52 +0000

    added Dockerfile to initialise postgresql environment

--- Dockerfile
+++ Dockerfile
@@ -3,4 +3,6 @@ FROM postgres:15-alpine
 LABEL author="Elliot"
 LABEL version="1.0"
 
+EXPOSE 5432
+
 COPY *.sql /docker-entrypoint-initdb.d/
---
+++ entry.sql
@@ -0,0 +1,7 @@
+CREATE TABLE example (
+    row1            varchar(80),
+    row2            int,           
+    row3            int,           
+    row4            real,         
+    row5            date
+);
